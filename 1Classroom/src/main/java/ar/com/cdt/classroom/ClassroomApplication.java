a4edf045478c8b2c49cc3c58c4e8f5a51cfef99d
AlbertoRanno <albertoranno@hotmail.com>
2024-07-08 18:22:08 -0300

    POM y LOG

---
+++ 1Classroom/src/main/java/ar/com/cdt/classroom/ClassroomApplication.java
@@ -0,0 +1,31 @@
+package ar.com.cdt.classroom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomApplication implements CommandLineRunner{
+    //Implemento, sobreescribiendo su unico metodo, run, para usar los LOGs, que tienen mas PROS que imprimir en consola
+
+    //Defino el log:
+    private static Logger LOG = LoggerFactory.getLogger(ClassroomApplication.class);
+    
+        //Metodo principal de la app
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomApplication.class, args);
+	}
+
+    //Sobreescribo el metodo run para la app    
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("INFO");
+        LOG.warn("WARN");
+        LOG.error("ERROR");
+        //Depende el IDE se ven en diferentes colores, pero aca no...
+    }
+    
+
+}
