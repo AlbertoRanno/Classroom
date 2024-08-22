8486c18309c1464008d2016a7d71c9daca236e61
AlbertoRanno <albertoranno@hotmail.com>
2024-08-22 13:34:35 -0300

    acomodando muchos comentarios en classroom6, iniciando classroom7 en limpio para implementar la idea de transacciones

---
+++ 7classroom-transacciones/src/main/java/ar/com/cdt/classroom/repository/IProfesorRepository.java
@@ -0,0 +1,7 @@
+package ar.com.cdt.classroom.repository;
+
+import ar.com.cdt.classroom.model.Profesor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProfesorRepository extends JpaRepository<Profesor, Integer> {
+}
